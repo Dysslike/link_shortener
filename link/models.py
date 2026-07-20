a20f70dfeff7ab21fd39cbb083e1f75f38d2099b
Dysslike <159203869+Dysslike@users.noreply.github.com>
2026-07-20 22:50:19 +0800

    Small Work not much today

--- link/models.py
+++ link/models.py
@@ -8,6 +8,7 @@ from django.db import models
 
 class User(models.Model):
     user_id = models.BigAutoField(primary_key=True)
+    username = models.CharField(max_length=30)
     email = models.EmailField(max_length=255, null=False)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30, blank=True, null=True )
